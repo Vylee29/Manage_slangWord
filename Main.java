5f3c2a6e5940d077d500d24c3496746463dcf283
Vylee <87347391+Vylee29@users.noreply.github.com>
2022-11-25 18:05:27 +0700

    update main

--- Main.java
+++ Main.java
@@ -1,19 +1,8 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) throws IOException {
-        slangWords sl = new slangWords();
-        Scanner sc = new Scanner(System.in);
-        int num;
-        sl.readFile();
-        do {
-            System.out.print("Input number you want:");
-            num = sc.nextInt();
-            if(num==1)
-            sl.findWithDef();
-            else if(num==2)
-            sl.takeHistoryList();
-        }while (num!=0);
-
+        gui Gui = new gui();
     }
 }
